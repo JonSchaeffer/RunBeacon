bb2091301a3302d8f71ecced2e49c514f250b970
JonSchaeffer <jschaeffer97@gmail.com>
2022-05-14 16:42:27 -0400

    Iteriating through the split

--- data/frontend/learningGo/gotut.go
+++ data/frontend/learningGo/gotut.go
@@ -2,14 +2,31 @@ package main
 
 import ("fmt"
 "net/http"
-"io/ioutil")
+"io/ioutil"
+"encoding/xml")
 
+type SitemapIndex struct {
+	Locations  []Location `xml:"url"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 
 func main(){
-	resp, _ := http.Get("https://www.cnn.com/sitemaps/cnn/index.xml")
+	resp, _ := http.Get("https://www.cnn.com/sitemaps/sitemap-section.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
 	resp.Body.Close()
 
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	//fmt.Println(s.Locations)
+
+	for _, Location := range s.Locations {
+		fmt.Printf("\n%s", Location)
+	}
 }
