b56738c2c77638cce3fe21353731f30fa9177235
JonSchaeffer <jschaeffer97@gmail.com>
2022-05-14 16:09:47 -0400

    Starting to build a web scraper/XML parser

--- data/frontend/learningGo/gotut.go
+++ data/frontend/learningGo/gotut.go
@@ -1,39 +1,15 @@
 package main
 
-import ("fmt")
+import ("fmt"
+"net/http"
+"io/ioutil")
 
-const usixteenbitman float64 = 65535
-const kmh_multiple float64 = 1.60934
 
-type car struct {
-	gas_pedal uint16 //min 0, max 65535
-	brake_pedal uint16
-	steering_wheel int16
-	top_speed_kmh float64
-}
+func main(){
+	resp, _ := http.Get("https://www.cnn.com/sitemaps/cnn/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	string_body := string(bytes)
+	fmt.Println(string_body)
+	resp.Body.Close()
 
-func (c car) kmh() float64 {
-	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitman)
-}
-
-func (c car) mph() float64 {
-	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitman/kmh_multiple)
-}
-
-func (c *car) new_top_speed(newspeed float64) {
-	c.top_speed_kmh = newspeed
-}
-
-func main() {
-	a_car := car{gas_pedal: 22431, 
-		brake_pedal: 0, 
-		steering_wheel: 12562, 
-		top_speed_kmh: 250.0}
-
-	fmt.Println(a_car.gas_pedal)
-	fmt.Println(a_car.kmh())
-	fmt.Println(a_car.mph())
-	a_car.new_top_speed(500)
-	fmt.Println(a_car.kmh())
-	fmt.Println(a_car.mph())
 }
